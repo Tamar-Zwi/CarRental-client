ec6e644cd2e0d7ba8ec0181882f68dc6544f2d2f
shira-azuelos <shira67254@gmail.com>
2026-03-25 17:32:57 +0200

    fff

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { CustomerLoginComponent } from './customer-login/customer-login';
 import { RegisterCustomerComponent } from './register-customer/register-customer'; 
 import { CarListComponent } from './car-list/car-list'; // הוספנו את הייבוא הזה
-
+//hhhhh
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: CustomerLoginComponent },
